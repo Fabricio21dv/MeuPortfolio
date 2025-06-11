e99ab564664aa17185e69caf52b3862d3e6fe427
Fabricio21dv <fabriciof.campos21@gmail.com>
2025-06-11 10:35:10 -0300

    adicionando os temas light e dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import Sidebar from './containers/Sidebar'
@@ -5,13 +6,20 @@ import Sobre from './containers/Sobre'
 import Projetos from './containers/Projetos'
 import EstiloGlobal, { Container } from './styles'
 import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <ThemeProvider theme={temaLight}>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { P } from '../../components/Paragrafo/styles'
+import { Theme } from '../../themes/dark'
 
 export const Descricao = styled(P)`
   margin-top: 24px;
@@ -12,8 +13,8 @@ export const BotaoTema = styled.button`
   padding: 8px;
   font-size: 10px;
   font-weight: bold;
-  color: #eee;
-  background-color: #282a35;
+  color: ${(props) => (props.theme as Theme).corDeFundo};
+  background-color: ${(props) => (props.theme as Theme).corPrincipal};
   cursor: pointer;
 `
 
@@ -21,4 +22,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
